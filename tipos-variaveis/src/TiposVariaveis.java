25c348310fae079a6e8d0cbeef4d724d8c8bd005
IDMarieCurie <65715776+IDMarieCurie@users.noreply.github.com>
2023-01-12 16:06:12 -0300

    Novos conteúdos de variáveis e operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
+    
+    final double VALOR_DE_PI = 3.14;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /*Exemplo de Casting  */
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;
+        
+        /*Alterando valor de variaáveis */
+            int numero = 5;
+
+            numero = 10;
+            System.out.print(numero);
+
+        /*Declaração de uma constante */
+            final double VALOR_DE_PI = 3.14;
+        
+        /*Para declarar textos */
+            String meuNome = "Vinicius Fernandes";
     }
 }
